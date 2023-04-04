b735520044d6db36405b79678573123c5b90ff78
EkaterinaRus <kateivan.89@mail.ru>
2023-04-05 00:34:17 +0300

    Добавления фильтра

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+import re
 
 register = template.Library()  # если мы не зарегестрируем наши фильтры, то django никогда не узнает
 # где именно их искать и фильтры потеряются
@@ -8,15 +9,8 @@ register = template.Library()  # если мы не зарегестрируем
 # что это именно фильтр, а не простая функция
 def censor(value, arg):  # первый аргумент здесь — это то значение, к которому надо применить фильтр,
     # второй аргумент — это аргумент фильтра, т.е. примерно следующее будет в шаблоне value|multiply:arg
-    # l = 'спартак'
-    if isinstance(value, str):  # проверяем, что value — это точно строка
-        if arg in value.lower():
-            v = value.lower().replace(arg, 'Реал')
-            return v
-        else:
-            return value
-    else:
-        raise value
-        # возвращаемое функцией значение — это то значение, которое подставится к нам в шаблон
+    res = re.sub(r'(?i)хуй(?=\W)', arg, value)
+    res1 = re.sub(r'(?i)блять(?=\W)', arg, res)
+    return res1
 
 
