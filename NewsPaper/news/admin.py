31e551a5dffc63fda8e644fec909aab585490680
Ekaterina Rusakova <kateivanrus89@gmail.com>
2023-07-02 01:07:00 +0300

    add ...

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,6 +1,7 @@
 # from django.contrib import admin
 
-from django.contrib import adminfrom .models import Post, Category, PostCategory, Comment, Author
+from django.contrib import admin
+from .models import Post, Category, PostCategory, Comment, Author
 
 
 class PostAdmin(admin.ModelAdmin):
